4f7d94037c9066899077c8e9293563289d81abc6
Italo <148975525+jtalo@users.noreply.github.com>
2024-12-28 20:16:10 -0300

    Add files via upload

---
+++ ex.sh
@@ -0,0 +1,7 @@
+# Exemplo:
+alias ls='ls -lh'
+
+## Componentes:
+valor    palavra-chave    atribuição       comando
+_____        ___             ___          __________
+alias        ls               =            'ls -lh'
